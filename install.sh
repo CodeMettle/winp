18af83107aa59e889b65d6404de2a6c3bc0250cb
Steven Scott <steven@codemettle.com>
2017-02-27 15:17:11 -0500

    publish a zip file with the kill executables

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -ex
+mkdir -p src/main/resources
 cp -v native/Release/winp.dll src/main/resources/winp.dll
 cp -v native/x64/Release/winp.dll src/main/resources/winp.x64.dll
+mkdir -p src/main/executables
+cp -v native/Release/winpkill.exe src/main/executables/winpkill.exe
+cp -v native/x64/Release/winpkill.exe src/main/executables/winpkill64.exe
